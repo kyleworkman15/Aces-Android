a444d1e16333fd729f80958542b4f6be546021da
kyleworkman15 <kyleworkman15@augustana.edu>
2018-09-07 11:32:24 -0500

    Fix notification bug

--- functions/index.js
+++ functions/index.js
@@ -7,47 +7,45 @@ let admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
-exports.sendNotification = functions.database.ref('/ACTIVE RIDES/{email}/notify').onWrite((change, context) => {
+exports.sendNotification = functions.database.ref('/ACTIVE RIDES/{email}/notify').onCreate((snapshot, context) => {
 	
-	//get the email + message
-	const receiverEmail = change.after.child('email').val();
-	const id = change.after.child('id').val();
-	var message = "Test";
-	var title = "Test";
-	if (id == 0) { // on the way notification
-		title = "Your ride is on the way!";
-		message = "Watch for the " + change.after.child('vehicle').val();
-	} else if (id == 1) { // here notification
-		title = "Your ride is here!";
-		message = "Hop in the " + change.after.child('vehicle').val();
-	}
+	if(snapshot.hasChildren()) {
+		//get the email + message
+		const id = snapshot.child('id').val();
+		const token = snapshot.child('token').val();
+		var message = "Test";
+		var title = "Test";
+		if (id == 0) { // on the way notification
+			title = "Your ride is on the way!";
+			message = "Watch for the " + snapshot.child('vehicle').val();
+		} else if (id == 1) { // here notification
+			title = "Your ride is here!";
+			message = "Hop in the " + snapshot.child('vehicle').val();
+		}
+
+		if (token != null) {
+			//send the message
+			console.log("Construction the notification message.");
+			const payload = {
+				notification: {
+					title: title,
+					body: message,
+					sound: 'default'
+				},
+				data: {
+					title: title,
+					message: message,
+					sound: 'default'
+				}
+			};
 	
-	//get the token of the user receiving the message
-	return admin.database().ref("/ACTIVE RIDES/" + receiverEmail).once('value').then(snap => {
-		const token = snap.child("token").val();
-		console.log("token: ", token);
-		
-		//send the message
-		console.log("Construction the notification message.");
-		const payload = {
-			notification: {
-				title: title,
-				body: message,
-				sound: 'default'
-			},
-			data: {
-				title: title,
-				message: message,
-				sound: 'default'
+			return admin.messaging().sendToDevice(token, payload)
+						.then(function(response) {
+							console.log("Successfully sent message:", response);
+				  		})
+				  		.catch(function(error) {
+							console.log("Error sending message:", error);
+				  		});
 			}
-		};
-		
-		return admin.messaging().sendToDevice(token, payload)
-					.then(function(response) {
-						console.log("Successfully sent message:", response);
-					  })
-					  .catch(function(error) {
-						console.log("Error sending message:", error);
-					  });
-	});
+	}
 });
